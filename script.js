5f11f6886fc8641687ce031fba038a10884d2ac7
ariannagreco3393 <a.greco3393@gmail.com>
2022-06-25 15:26:49 +0200

    add selection html tag

---
+++ script.js
@@ -0,0 +1,10 @@
+//seleziono elementi dall'html
+const rootElement = document.documentElement;
+const weatherIcon = document.querySelector('.weather-icon')
+const weatherLocation = document.querySelector('.weather-location')
+const weatherTemperature = document.querySelector('.weather-temperature')
+
+
+
+
+      
